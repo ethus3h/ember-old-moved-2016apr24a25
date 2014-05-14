8dde6c2b130802255402088c655b297d8bdad641
worldpeacehaven@gmail.com <worldpeacehaven@gmail.com@41ca91dd-718b-b11f-0e46-dcd1c118863d>
2014-05-14 21:06:33 +0000

    This is all Ember now

--- trunk/ember.php
+++ trunk/ember.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set("display_errors", true);
 $displayDebugMessages = True;
 #Ember.
-$emberVersion = 'Version 0.91.15, 14 May 2014.';
+$emberVersion = 'Version 8-0.91.15, 14 May 2014.';
 
 include('active.php');
 
