295d9409cc40fdb76b0f71a964cb7476a4b86dcb
worldpeacehaven <ethus3h@users.noreply.github.com>
2014-05-15 19:54:00 +0000

    continuing to get rid of some warning messages

--- w.classes.php
+++ w.classes.php
@@ -5,43 +5,43 @@ include ('d/r/wc.baggage-claim.php');
 #Properties: text
 include ('d/r/wc.text.php');
 #Properties: long text
-include ('d/r/wc.longtext.php');
+//include ('d/r/wc.longtext.php');
 #Properties: number
-include ('d/r/wc.number.php');
+//include ('d/r/wc.number.php');
 #Properties: reference
-include ('d/r/wc.reference.php');
+//include ('d/r/wc.reference.php');
 #Properties: Boolean
-include ('d/r/wc.boolean.php');
+//include ('d/r/wc.boolean.php');
 #Properties: data
-include ('d/r/wc.data.php');
-include ('d/r/wc.datarev.php');
-include ('d/r/wc.datatype.php');
+//include ('d/r/wc.data.php');
+//include ('d/r/wc.datarev.php');
+//include ('d/r/wc.datatype.php');
 
 #GENERAL RECORD TYPES
 #Interface fragments
 include ('d/r/wc.intf.php');
 #Locales
-include ('d/r/wc.locale.php');
+//include ('d/r/wc.locale.php');
 #Characters
-include ('d/r/wc.character.php');
+//include ('d/r/wc.character.php');
 #Character categories
-include ('d/r/wc.charcat.php');
+//include ('d/r/wc.charcat.php');
 #Scripts
-include ('d/r/wc.script.php');
+//include ('d/r/wc.script.php');
 #Dates and times
-include ('d/r/wc.datetime.php');
+//include ('d/r/wc.datetime.php');
 #Users
 include ('d/r/wc.user.php');
 
 #ABSTRACT RECORD TYPES
 #Nodes
-include ('d/r/wc.node.php');
-include ('d/r/wc.noderev.php');
-include ('d/r/wc.nodetype.php');
+//include ('d/r/wc.node.php');
+//include ('d/r/wc.noderev.php');
+//include ('d/r/wc.nodetype.php');
 #Relationships
-include ('d/r/wc.rel.php');
-include ('d/r/wc.relrev.php');
-include ('d/r/wc.reltype.php');
+//include ('d/r/wc.rel.php');
+//include ('d/r/wc.relrev.php');
+//include ('d/r/wc.reltype.php');
 #Metadata
-include ('d/r/wc.meta.php');
+//include ('d/r/wc.meta.php');
 ?>
