fa206d1fbb4b7d3ef163fcd5e7bb1224be879016
worldpeacehaven@gmail.com <worldpeacehaven@gmail.com@41ca91dd-718b-b11f-0e46-dcd1c118863d>
2014-05-24 08:13:07 +0000

    hmm

--- trunk/ember.php
+++ trunk/ember.php
@@ -1,8 +1,11 @@
 <?php
 //"Far off in the distance, a fire comes to light — a flicker of a flame breaks through the dark, burning soft and bright, dancing with the light, awakening the heavens and the Earth" … Ember
 //error_reporting(E_ALL);
-ini_set("display_errors", false);
-$displayDebugMessages = True;
+ini_set("display_errors", 0);
+//from http://www.php.net/manual/en/function.error-reporting.php
+// Turn off all error reporting
+error_reporting(0);
+$displayDebugMessages = False;
 #Ember.
 $emberVersion = 'Version 8-0.91.21, 23 May 2014 and 24 May 2014 a.mn..';
 $error = 0;
