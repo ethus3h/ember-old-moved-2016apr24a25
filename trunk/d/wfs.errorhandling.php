fffaac4cb4b60c5e26d0c7c6cc7f7f5f7fc4ae64
worldpeacehaven@gmail.com <worldpeacehaven@gmail.com@41ca91dd-718b-b11f-0e46-dcd1c118863d>
2014-06-09 00:18:45 +0000

    More work on Ember; add'l resources

---
+++ trunk/d/wfs.errorhandling.php
@@ -0,0 +1,6 @@
+<?php
+//Handle errors
+include('d/r/wf.err.php');
+//Handle 'unrecognised action' errors
+include('d/r/wf.unrecognisedAction.php');
+?>
---
+++ trunk/d/wfs.utility.php
@@ -0,0 +1,7 @@
+<?php
+//Utility functions
+//explode_escaped (not written by me)
+include('d/r/wf.explode_escaped.php');
+//Return an escaped array (not written by me)
+include('d/r/wf.mysql_real_escape_array.php');
+?>
