9aa5d91178f7a9aeaf82c64ddf77217d0d8ed603
worldpeacehaven@gmail.com <worldpeacehaven@gmail.com@41ca91dd-718b-b11f-0e46-dcd1c118863d>
2014-06-12 03:58:29 +0000

    Hmm!

--- trunk/ember.php
+++ trunk/ember.php
@@ -7,7 +7,7 @@ ini_set("display_errors", 0);
 error_reporting(0);
 $displayDebugMessages = False;
 #Ember.
-$emberVersion = 'Version 8-0.91.34, 11 June 2014 a.mn..';
+$emberVersion = 'Version 8-0.91.35, 11 June 2014.';
 $error = 0;
 include('Crypt/RSA.php');
 include('active.php');
