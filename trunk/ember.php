9423136dd05b645260891458c8848c18239ff73e
worldpeacehaven@gmail.com <worldpeacehaven@gmail.com@41ca91dd-718b-b11f-0e46-dcd1c118863d>
2014-06-27 22:24:00 +0000

    Ok, maybe the up load is working! :D

--- trunk/ember.php
+++ trunk/ember.php
@@ -7,7 +7,7 @@ ini_set("display_errors", 0);
 error_reporting(0);
 $displayDebugMessages = False;
 #Ember.
-$emberVersion = 'Version 8-0.91.39, 26 June 2014.';
+$emberVersion = 'Version 8-0.91.40, 27 June 2014.';
 $error = 0;
 include('Crypt/RSA.php');
 include('active.php');
