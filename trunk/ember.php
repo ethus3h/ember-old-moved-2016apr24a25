30b4be7fa8ca9d572944b264c55c2fe60c007f59
worldpeacehaven@gmail.com <worldpeacehaven@gmail.com@41ca91dd-718b-b11f-0e46-dcd1c118863d>
2014-07-13 23:39:32 +0000

    A philosophy doohicky.

--- trunk/ember.php
+++ trunk/ember.php
@@ -7,7 +7,7 @@ ini_set("display_errors", 0);
 error_reporting(0);
 $displayDebugMessages = False;
 #Ember.
-$emberVersion = 'Version 8-0.91.43, 12 July 2014 and 13 July 2014 a.mn..';
+$emberVersion = 'Version 8-0.91.44, 13 July 2014.';
 $error = 0;
 include('Crypt/RSA.php');
 include('active.php');
