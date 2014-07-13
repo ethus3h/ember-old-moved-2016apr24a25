1126b7687272ce5b3f08980b8c6c099ca74d243c
worldpeacehaven <ethus3h@users.noreply.github.com>
2014-07-13 03:56:42 +0000

    20 possibilities, i think

--- ember.php
+++ ember.php
@@ -7,7 +7,7 @@ ini_set("display_errors", 0);
 error_reporting(0);
 $displayDebugMessages = False;
 #Ember.
-$emberVersion = 'Version 8-0.91.42, 29 June 2014. Note that I forgot to update this version number and date yesterday, so there is no version 8-0.91.41, really.';
+$emberVersion = 'Version 8-0.91.43, 12 July 2014.';
 $error = 0;
 include('Crypt/RSA.php');
 include('active.php');
