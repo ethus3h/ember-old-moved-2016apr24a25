3c1f30027feb3af9d7730252f9237b254e37f61a
worldpeacehaven@gmail.com <worldpeacehaven@gmail.com@41ca91dd-718b-b11f-0e46-dcd1c118863d>
2014-07-13 06:21:05 +0000

    Maybe this will fix this situation....

--- trunk/active.functions.extra.php
+++ trunk/active.functions.extra.php
@@ -452,7 +452,7 @@ function coalFromFile($filename,$csump) {
 function checkCoal($id) {
 	global $l;
 	$l->a('Begun checking coal...');
-	sleep(2);
+	sleep(12);
 	$coal = retrieveCoal($id);
 	$l->a('checkCoal returned status: '.$coal['status'].'.<br>');
 	return check($coal['status']);
