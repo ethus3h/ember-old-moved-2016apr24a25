f4f621f88fc68345448189a10f8864680b593265
worldpeacehaven@gmail.com <worldpeacehaven@gmail.com@41ca91dd-718b-b11f-0e46-dcd1c118863d>
2014-08-18 22:08:48 +0000

    ...

--- trunk/src/main/java/com/futuramerlin/ember/Server/Server.java
+++ trunk/src/main/java/com/futuramerlin/ember/Server/Server.java
@@ -1,5 +1,11 @@
 package com.futuramerlin.ember.Server;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+
 /**
  * Created by PermissionGiver on 8/16/14.
  */
@@ -10,5 +16,14 @@ public class Server {
 
     public void print(String s) {
         System.out.println(s);
+    }
+
+    public void createHttpServerInstance() throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(80),0);
+    }
+
+    public void createHttpLoopbackServerInstance() throws IOException {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(InetAddress.getLoopbackAddress(), 80),0);
     }
 }
