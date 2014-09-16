8f13fcc9ae270d04cb1da39907009bc2cf7807dd
worldpeacehaven <ethus3h@users.noreply.github.com>
2014-09-16 01:02:13 +0000

    ...ok

--- src/main/java/com/futuramerlin/ember/Server/MetaServer/MetaServer.java
+++
@@ -1,9 +0,0 @@
-package com.futuramerlin.ember.Server.MetaServer;
-
-import com.futuramerlin.ember.Server.Server;
-
-/**
- * Created by PermissionGiver on 8/16/14.
- */
-public class MetaServer extends Server {
-}
--- src/main/java/com/futuramerlin/ember/Throwable/CorruptedSafeDataException.java
+++
@@ -1,7 +0,0 @@
-package com.futuramerlin.ember.Throwable;
-
-/**
- * Created by PermissionGiver on 8/18/14.
- */
-public class CorruptedSafeDataException extends Throwable {
-}
