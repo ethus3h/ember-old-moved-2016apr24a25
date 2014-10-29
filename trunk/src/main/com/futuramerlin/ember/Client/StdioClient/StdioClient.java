92bf913c040678d9c36cc9ccbeef1145274e12e7
worldpeacehaven@gmail.com <worldpeacehaven@gmail.com@41ca91dd-718b-b11f-0e46-dcd1c118863d>
2014-10-29 06:54:49 +0000

    hmm!

--- trunk/src/main/com/futuramerlin/ember/Client/StdioClient/StdioClient.java
+++ trunk/src/main/com/futuramerlin/ember/Client/StdioClient/StdioClient.java
@@ -4,4 +4,10 @@ package com.futuramerlin.ember.Client.StdioClient;
  * Created by elliot on 14.10.29.
  */
 public class StdioClient {
+    public static void main() {
+
+    }
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
 }
