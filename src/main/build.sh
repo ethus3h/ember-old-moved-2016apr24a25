1b5ced377e013f07cf72b990db2162a3c5f9ced0
worldpeacehaven <ethus3h@users.noreply.github.com>
2014-10-30 06:34:15 +0000

    It compiles! :D
    Still doesn't run, but oh well. That's a problem for another day, I suppose.

--- src/main/build.sh
+++ src/main/build.sh
@@ -1,7 +1,17 @@
-compile() {
-	echo "Trying to build $1..."
-	javac -cp ../../lib ./$1.java
-	jar cfe $1.jar $1 .
+#!/bin/bash
+echo "Trying to build Ember..."
+echo "Cleaning any existing files..."
+rm -rf *.class
+echo "Done cleaning"
+echo "Compiling..."
+emcompile() {
+	echo "Trying to build module: $1..."
+	javac -cp ../../lib ./com/futuramerlin/ember/${1/./\/}/${1#*.}.java
+	jar cfe $1.jar com.futuramerlin.ember.$1 .
 	echo "Your $1 is now ready in $1.jar."
 }
 
+emcompile "Client.StdioClient";
+
+
+echo "Done!"
