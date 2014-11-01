ca4b3c283fe9db7b18fd54d76269ad278966cd35
worldpeacehaven@gmail.com <worldpeacehaven@gmail.com@41ca91dd-718b-b11f-0e46-dcd1c118863d>
2014-11-01 06:03:40 +0000

    assert is usually disabled.

--- trunk/src/test/com/futuramerlin/ember/Client/ClientTest.java
+++ trunk/src/test/com/futuramerlin/ember/Client/ClientTest.java
@@ -3,12 +3,16 @@ package com.futuramerlin.ember.Client;
 import com.futuramerlin.ember.ApiClient.ApiClient;
 import com.futuramerlin.ember.Client.Client;
 //import org.junit.Test;
-import org.junit.Assert;
+//import org.junit.Assert;
 
 /**
  * Created by elliot on 14.10.29.
  */
 public class ClientTest {
+    public static void main(String[] args) throws Exception {
+        ClientTest c = new ClientTest();
+        c.testClientGetNewApiClientAssigns();
+    }
  /*   @Test
     public void testCreateStdioClient() throws Exception {
         Client c = new Client();
