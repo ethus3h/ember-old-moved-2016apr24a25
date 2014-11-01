e39ea25a3c436c201479d7ebc163a9d6cf8bd0b2
worldpeacehaven@gmail.com <worldpeacehaven@gmail.com@41ca91dd-718b-b11f-0e46-dcd1c118863d>
2014-11-01 05:44:34 +0000

    w00t! non-static can't be referenced error fixed!

--- trunk/src/main/com/futuramerlin/ember/Client/Client.java
+++ trunk/src/main/com/futuramerlin/ember/Client/Client.java
@@ -1,14 +1,30 @@
 package com.futuramerlin.ember.Client;
 
+import com.futuramerlin.ember.ApiClient.ApiClient;
+
 /**
  * Created by elliot on 14.10.29.
  */
 public class Client {
+    public ApiClient apiClient;
+
     public static void main(String[] args) {
         Client c = new Client();
         c.sayHello();
+        //c.getNewApiClient();
     }
     public void sayHello() {
-        System.out.println("Hello!");
+        System.out.println("Hello! Ember is now starting. It may take a little while for it to do so.");
     }
+/*
+    public ApiClient getNewApiClient() {
+        return new ApiClient();
+    }
+
+    public ApiClient getApiClient() {
+        if(this.apiClient == null) {
+            return new ApiClient();
+        }
+        return this.apiClient;
+    } */
 }
