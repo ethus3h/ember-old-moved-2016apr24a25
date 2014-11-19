14dec89598ed71ab5edff26cbb5e0e75734d30b4
worldpeacehaven <ethus3h@users.noreply.github.com>
2014-11-19 05:42:35 +0000

    ok

---
+++ projects/gmg/game/src/gameobjects/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+"""
+Game objects that can be created and placed somewhere in a map
+"""
+
+from testobject import TestObject
+from camera import Camera
+from playerobject import Player
+from energy import Energy
+from playerlaser import PlayerLaser
+from crawler import Crawler
+from explosion import Explosion
+from spawner import Spawner
+from despawner import Despawner
+from image import Image, AnimImage
+from door import Door
+from cave import Cave
+from sign import Sign
+from sidetravel import SideTravel
+from chickbot import ChickBot
+from outhouse import Outhouse
+from startspawn import StartSpawn
+from npc import NPC
+from steam import Steam
+from helibot import HeliBot
