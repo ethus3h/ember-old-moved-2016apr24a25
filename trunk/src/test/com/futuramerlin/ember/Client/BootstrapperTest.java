53312ab205b6d9c5d014e7260bb750fb3f5bf6b3
worldpeacehaven@gmail.com <worldpeacehaven@gmail.com@41ca91dd-718b-b11f-0e46-dcd1c118863d>
2014-11-27 19:48:52 +0000

    ...

--- trunk/src/test/com/futuramerlin/ember/Client/BootstrapperTest.java
+++ trunk/src/test/com/futuramerlin/ember/Client/BootstrapperTest.java
@@ -4,7 +4,12 @@ import com.futuramerlin.ember.Client.ApiClient.ApiClient;
 import com.futuramerlin.ember.Common.Exception.ApiClientAlreadyExistsException;
 import com.futuramerlin.ember.Common.Exception.NoTerminalFoundException;
 import com.futuramerlin.ember.Common.Exception.ZeroLengthInputException;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 /**
  * Created by elliot on 14.11.01.
@@ -14,6 +19,7 @@ public class BootstrapperTest {
         BootstrapperTest c = new BootstrapperTest();
         c.testBootstrapperGetNewApiClientAssigns();
     }
+
     @Test
     public void testBootstrapperRun() throws Exception {
         Bootstrapper b = new Bootstrapper();
@@ -123,6 +129,48 @@ public class BootstrapperTest {
     public void testMessage() throws Exception {
         Bootstrapper c = new Bootstrapper();
         c.message("");
+
+    }
+
+    @Test
+    public void testProcessSignalHandler() throws Exception {
+        Bootstrapper c = new Bootstrapper();
+        c.processSignalHandler(1);
+
+    }
+
+    @Test
+    public void testRun() throws Exception {
+        Bootstrapper c = new Bootstrapper();
+        c.run();
+        org.junit.Assert.assertEquals(c.term,System.console());
+
+    }
+    //help from http://stackoverflow.com/questions/1119385/junit-test-for-system-out-println
+/*    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+        System.setErr(null);
+    } */
+    @Test
+    public void testCatchApiClientAlreadyExists() throws Exception {
+        Bootstrapper c = new Bootstrapper();
+        c.apiClient = c.getApiClient();
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        c.run();
+        org.junit.Assert.assertEquals("Failed to create: ApiClient already exists.",outContent.toString());
+
 
     }
 }
