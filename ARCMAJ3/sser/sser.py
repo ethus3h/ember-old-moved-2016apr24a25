29c568659a7f2316fd6cb3263ceefd7e66b70027
worldpeacehaven <ethus3h@users.noreply.github.com>
2014-11-30 19:16:51 +0000

    Workingish. :)

--- ARCMAJ3/sser/sser.py
+++ ARCMAJ3/sser/sser.py
@@ -53,6 +53,8 @@ import os
 import uuid
 import time
 import urllib
+import traceback
+import subprocess
 
 print 'Note that this app should have at LEAST 2x the size of the biggest file of free space.'
 ad = ''
@@ -70,6 +72,8 @@ if len(ad) < 1:
 	os.system('mkdir ../Archive.sserdb/ehdb/')
 	os.system('mkdir ../Archive.sserdb/encdb/')
 	os.system('mkdir ../Archive.sserdb/hashesdb/')
+	os.system('mkdir ./Meta/')
+	os.system('mkdir ./Meta/Revisions/')
 	ad = '0';
 	print 'Initializing new sser repository'
 	ax = open('../Archive.sserdb/meta/latest','wb')
@@ -89,32 +93,56 @@ if len(ad) < 1:
 	ax = open('../Archive.sserdb/meta/passphrase','wb')
 	ax.write(pp)
 	ax.close()
-		
+
+ltime = time.strftime("%Y.%m.%d.%H.%M.%S.%f.%z", time.gmtime())		
 
 #Command definitions
 errored = False
+def check_output(*popenargs, **kwargs):
+	r"""Run command with arguments and return its output as a byte string.
+
+	Backported from Python 2.7 as it's implemented as pure python on stdlib.
+
+	>>> check_output(['/usr/bin/python', '--version'])
+	Python 2.6.2
+	"""
+	process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+	output, unused_err = process.communicate()
+	retcode = process.poll()
+	if retcode:
+		cmd = kwargs.get("args")
+		if cmd is None:
+			cmd = popenargs[0]
+		error = subprocess.CalledProcessError(retcode, cmd)
+		error.output = output
+		raise error
+	return output
+def log_add(text):
+    text = str(text)
+    print text
+    global ltime
+    f = open('log-'+ltime+'.log', 'a')
+    f.write(text+"\n")
+    f.close()
 def run(command):
-    print command
+    log_add("Running: "+command)
     global errored
     commandResult = ''
     try:
         commandRes=check_output(command, shell=True, stderr=subprocess.STDOUT)
-        commandResult = "Running command: \n\n" + command + "\n\n\n\n" + commandRes + "\n\n\n\n"
+        commandResult = "Running command: \n" + command + "\n" + commandRes + "\n"
     except Exception, e:
+    	# http://stackoverflow.com/questions/9555133/e-printstacktrace-equivalent-in-python
+    	traceback.print_exc()
         commandRes=''
         try:
-            commandResult = "Running command: \n\n" + command + "\n\n\n\n" + commandResult + str(e.output) + "\n\n\n\nError encountered while running command. This is probably not a big deal.\n\n"
+            commandResult = "Running command: \n" + command + "\n" + commandResult + str(e.output) + "\nError encountered while running command. This is probably not a big deal."
         except Exception, e:
-            commandResult = "\n\n\n\nError encountered while running command: \n\n" + command + "\n\n\n\nThis is probably not a big deal. Possibly the command line was incorrectly structured?\n\n"
+            commandResult = "Error encountered while running command: \n" + command + "\nThis is probably not a big deal. Possibly the command line was incorrectly structured?"
         errored=True
+    log_add('Command result: '+commandResult+'\n')
+    log_add('Command output: '+commandRes+'\n')
     return [commandResult,commandRes]
-def log_add(text):
-    text = str(text)
-    print text
-    global time
-    f = open('log-'+time+'.log', 'a')
-    f.write(text+"\n")
-    f.close()
 #Done command definitions
 
 previousRevision=int(open('../Archive.sserdb/meta/latest', 'r').readlines()[0])
@@ -124,14 +152,18 @@ accesskey = open('../Archive.sserdb/meta/conf', 'r').readlines()[0].strip()
 secretkey = open('../Archive.sserdb/meta/conf', 'r').readlines()[1].strip()
 passphrase = open('../Archive.sserdb/meta/passphrase', 'r').readlines()[0].strip()
 collection = 'coalproject'
-time = time.strftime("%Y.%m.%d.%H.%M.%S.%f.%z", time.gmtime())
 
-run('mv -v ../Archive.sserdb/snapshots/'+thisRevision+' ../Archive.sserdb/snapshots/disabled/'+thisRevision+'_'+time)
-run('mkdir ../Archive.sserdb/snapshots/'+thisRevision)
+try:
+	run('mv -v ../Archive.sserdb/snapshots/'+thisRevision+' ../Archive.sserdb/snapshots/disabled/'+thisRevision+'_'+ltime)
+except:
+	pass
 errored = False #doesn't matter if there were errors before here
+run('mkdir -v ../Archive.sserdb/snapshots/'+thisRevision)
+run('mkdir -v ../Archive.sserdb/snapshots/'+thisRevision+'/d/')
 
 #Start getting time
-os.system('echo "'+time+'" > ../Archive.sserdb/snapshots/'+thisRevision+'/localTime')
+run('pwd')
+run('echo "'+ltime+'" > ../Archive.sserdb/snapshots/'+thisRevision+'/localTime')
 timefile = urllib.URLopener()
 timefn = '../Archive.sserdb/snapshots/'+thisRevision+'/remoteTime'
 try:
@@ -152,6 +184,7 @@ records = []
 for dirpath, dirs, files in os.walk('Test'):
 	with open(os.path.join(dirpath, filename)) as f:
 		records.append([f,os.popen('shasum --algorithm 512 '+f),filename])
+c = 0
 for item in records:
 	if os.path.isfile('../Archive.sserdb/hashesdb/'+item[1]):
 		break; # file already in repo
@@ -171,13 +204,13 @@ for item in records:
         title = "Colistarr Initiative: sser_db "+uuid+" rev. "+thisRevision
         description = "Colistarr Initiative: sser_db "+uuid+" rev. "+thisRevision
         keywords = ['Colistarr','Colistarr Initiative','sser','sser_db','archive','snapshot', title]                        
-    	barrelSize = int(os.path.getsize(dump))
+    	barrelSize = int(os.path.getsize(item[0]))
         curl = ['curl', '--location', 
         	'--retry', '999',
         	'--retry-max-time', '0',
             '--header', "'x-amz-auto-make-bucket:1'", # Creates the item automatically, need to give some time for the item to correctly be created on archive.org, or everything else will fail, showing "bucket not found" error
             '--header', "'x-archive-queue-derive:0'",
-            '--header', "'x-archive-size-hint:%d'" % (os.path.getsize(dump)), 
+            '--header', "'x-archive-size-hint:%d'" % (os.path.getsize(item[0])), 
             '--header', "'authorization: LOW %s:%s'" % (accesskey, secretkey),
         ]
         if c == 0:
@@ -185,8 +218,8 @@ for item in records:
                 '--header', "'x-archive-meta-collection:%s'" % (collection),
                 '--header', "'x-archive-meta-title:%s'" % (title),
                 '--header', "'x-archive-meta-description:%s'" % (description),
-                '--header', "'x-archive-meta-subject:%s'" % ('; '.join(wikikeys)), # Keywords should be separated by ; but it doesn't matter much; the alternative is to set one per field with subject[0], subject[1], ...
-                '--header', "'x-archive-meta-mediatype:data'",
+                '--header', "'x-archive-meta-subject:%s'" % ('; '.join(keywordvision
+keywords = ['Colistarr','Colistarr Initiative','sser','sser_snapshot','archive','snapshot', uuid, uuid+" rev. "+thisRevision, title]                   '--header', "'x-archive-meta-mediatype:data'",
             ]
         curl += ['--upload-file', "%s" % (item[0]),
                 "http://s3.us.archive.org/" + identifier + '/' + item[2] # It could happen that the identifier is taken by another user; only wikiteam collection admins will be able to upload more files to it, curl will fail immediately and get a permissions error by s3.
@@ -204,6 +237,7 @@ for item in records:
             log_add('ERROR UPLOADING FILE. THIS IS NOT GOOD.')
             sys.exit()
         errored = False
+        c = c+1
         #Done upload to IA
         
         run('echo "'+hash+'" > ../Archive.sserdb/ehdb/'+encHash)
@@ -213,34 +247,32 @@ run('find . -type d -exec mkdir -p ../Archive.sserdb/snapshots/'+thisRevision+'/
 # 6. foreach {records} as {item}:
 #   I. {item.name} <- "http://archive.org/download/Collistar_sser_pack_{../Archive.sserdb/uuid}_{{../Archive.sserdb/latest}++}/{enctmp.sha512()}"
 # 7. Hardlink ../Archive.sserdb/snapshots/{{../Archive.sserdb/latest}++}/idx/ehdb/ to ../Archive.sserdb/ehdb/
-# 8. Hardlink ../Archive.sserdb/snapshots/{{../Archive.sserdb/latest}++}/idx/hashesdb/ to ../Archive.sserdb/hashesdb/
+# 8. Hardlink ../Archive.sserdb/snapshots/{{../Archive.sserdb/latest}++}/idx/hashesdb/ to ../Archlive.sserdb/hashesdb/
 # 9. ../.tmp.{../Archive.sserdb/uuid}.{time} <- ../Archive.sserdb/snapshots/{{../Archive.sserdb/latest}++}/.pax().bzip2().aes256()
-# 10. Upload ../.tmp.{../Archive.sserdb/uuid}.{time} to ia:Collistar_sser_pack_{../Archive.sserdb/uuid}_{{../Archive.sserdb/latest}++}
-# 11. ../.tmp.{../Archive.sserdb/uuid}.{time}
+# 10. Upload ../.tmp.{../Archive.sserdb/uuid}ltime)
+run('rm -v ../Archive.sserdb/.tmp.'+uuid+'.'+ltime)
+run('mv -v ../Archive.sserdb/.tmp.'+uuid+'.'+lp.{../Archive.sserdb/uuid}.{time}
 # 12. Move ../.tmp.{../Archive.sserdb/uuid}.{time} to ./Meta/Revisions/{{../Archive.sserdb/latest}++}.sserrev
-# 13. ../Archive.sserdb/latest++;
+# 13. ../Archiv./Meta/Revisions/'+thisRevision+'.sserrev'test++;
 for records as item:
 	run('echo "http://archive.org/download/Collistar_sser_db_'+uuid+'_'+thisRevision+'/'+encHash+'" > ../Archive.sserdb/snapshots/'+thisRevision+'/d/'+item[0])
 run('ln ../Archive.sserdb/snapshots/'+thisRevision+'/idx/ehdb/ ../Archive.sserdb/ehdb/')
-run('ln ../Archive.sserdb/snapshots/'+thisRevision+'/idx/hashesdb/ ../Archive.sserdb/hashesdb/')
+run('ln ../Archive.sserdb/snapshots/'+thisRevision+'/idx/hashesdb/ ../Archive.sserdb/ha'./Meta/Revisions/'+thisRevision+'.sserrev'db/')
 run('ln ../Archive.sserdb/snapshots/'+thisRevision+'/meta/ ../Archive.sserdb/meta/')
 run('tar -cvj --format pax -f ../Archive.sserdb/.tmp.'+uuid+'.'+time+' -C .. '+'../Archive.sserdb/snapshots/'+thisRevision+'/')
 run('gpg --yes -c --cipher-algo AES256 --batch --passphrase-file ../Archive.sserdb/meta/passphrase ../Archive.sserdb/.tmp.'+uuid+'.'+time)
-run('rm -v ../Archive.sserdb/.tmp.'+uuid+'.'+time)
-run('mv -v ../Archive.sserdb/.tmp.'+uuid+'.'+time+'.gpg ./Meta/Revisions/'+thisRevision+'.sserrev')
-
-#Starting upload to IA
-identifier='Collistar_sser_snapshot_'+uuid+'_'+thisRevision
-log_add('Uploading: ' + item[0])
-time.sleep(0.1)
-title = "Colistarr Initiative: sser_snapshot "+uuid+" rev. "+thisRevision
-description = "Colistarr Initiative: sser_snapshot "+uuid+" rev. "+thisRevision
-keywords = ['Colistarr','Colistarr Initiative','sser','sser_snapshot','archive','snapshot', uuid, uuid+" rev. "+thisRevision, title]                        
-barrelSize = int(os.path.getsize(dump))
-curl = ['curl', '--location', 
-	'--retry', '999',
-	'--retry-max-time', '0',
-	'--header', "'x-amz-auto-make-bucket:1'", # Creates the item automatically, need to give some time for the item to correctly be created on archive.org, or everything else will fail, showing "bucket not found" error
+run('rm -v ../Archive.sserdb/.tmp.'+uui'./Meta/Revisions/'+thisRevision+'.sserrev')), 
+	'--header', "'authorization: LOW %s:%s'" % (accesskey, secretkey),
+]
+curl += ['--header', "'x-archive-meta-mediatype:data'",
+	'--header', "'x-archive-meta-collection:%s'" % (collection),
+	'--header', "'x-archive-meta-title:%s'" % (title),
+	'--header', "'x-archive-meta-description:%s'" % (description),
+	'--header', "'x-archive-meta-subject:%s'" % ('; '.join(keywordvision
+keywords = ['Colistarr','Colistarr Initiative','sser','sser_snapshot','archive','snapshot', uuid, uuid+" rev. "+thisRevision, title]    '--header', "'x-archive-meta-mediatype:data'",
+]
+curl += ['--upload-file', "%s" % ('./Meta/Revisions/'+thisRevision+'.sserrev'),
+		"http://s3.us.archive.org/" + identifier + '/' + thisRevision+'.sserrev' # It could happen that the identifier is taken by another user; only wikiteam collection admins will be able to upload more files to it, curl will fail immediately and get a permissions error by s3.
 	'--header', "'x-archive-queue-derive:0'",
 	'--header', "'x-archive-size-hint:%d'" % (os.path.getsize(dump)), 
 	'--header', "'authorization: LOW %s:%s'" % (accesskey, secretkey),
