2d01f12fef5399d0d5f23c11a4c4003c62d49e1d
worldpeacehaven@gmail.com <worldpeacehaven@gmail.com@41ca91dd-718b-b11f-0e46-dcd1c118863d>
2014-12-08 05:16:23 +0000

    ok

--- trunk/More/iarqh.php
+++ trunk/More/iarqh.php
@@ -1,20 +1,27 @@
 <?php
 //Request handler
+error_reporting(E_ALL);
+ini_set("display_errors", true);
 $rq = $_REQUEST['data'];
-echo 'Search results for: $rq';
-$db = new PDO('mysql:host=localhost;dbname=futuqiur_iaidx;charset=utf8', 'iaidx', 'Artemis!');
-$results = $db->query('SELECT * FROM `data` WHERE `filename` LIKE \'%'.$rq.'%\' LIMIT 10');
+echo 'Search results for: '.$rq;
+$db = new PDO('mysql:host=localhost;dbname=futuqiur_iaidx;charset=utf8', 'futuqiur_iaidx', 'Artemis!');
+$query = 'SELECT * FROM `data` WHERE `filename` LIKE \'%'.$rq.'%\' LIMIT 10';
+echo 'Running query:'.$query;
+$results = $db->query($query);
 $resultListEntryA= '<ul><li><a href="'; # url
 $resultListEntryB= '"><b>'; #title
 $resultListEntryC = '</b><br>'; # url
-$resultListEntryD = '<i>SHA: '; # SHA
+$resultListEntryD = '<br><i>SHA: '; # SHA
 $resultListEntryE = '</i></a></li></ul>';
-foreach($results as $result) {
-	$res = $result.explode();
+foreach($results as $res) {
 	echo $resultListEntryA;
 	echo $res['url'];
 	echo $resultListEntryB;
-	echo substr($res['url'],strrchr($res['url'],'/'));
+	$fn = strrchr($res['url'],'/');
+	if($fn = '') {
+	$fn = $res['url'];
+	}
+	echo $fn;
 	echo $resultListEntryC;
 	echo $res['url'];
 	echo $resultListEntryD;
