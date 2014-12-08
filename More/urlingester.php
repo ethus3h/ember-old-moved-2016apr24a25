53898594be62cd9c091b52d0913d40f9a9ed5c02
worldpeacehaven <ethus3h@users.noreply.github.com>
2014-12-08 06:43:54 +0000

    yeah... reggae hell

--- More/urlingester.php
+++ More/urlingester.php
@@ -5,9 +5,8 @@ ini_set("display_errors", true);
 $rq = $_REQUEST['urls'];
 echo 'Ingesting urls: '.$rq;
 $db = new PDO('mysql:host=localhost;dbname=futuqiur_iaidx;charset=utf8', 'futuqiur_iaidx', 'Artemis!');
-$rqn = str_replace($rq,"\r","\n");
-$rqnt = str_replace($rq,"\n\n","\n");
-foreach(explode("\n",$rqnt) as $line) {
+#next line from http://stackoverflow.com/questions/1462720/iterate-over-each-line-in-a-string-in-php
+foreach(preg_split("/((\r?\n)|(\r\n?))/", $rq) as $line){
 echo 'Line: '.$line;
 $la = explode(';',$line);
 $url = $la[0];
