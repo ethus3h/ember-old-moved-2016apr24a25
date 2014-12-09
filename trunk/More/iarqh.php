799d11d7383a33c793d68d698717bcca066c3ad4
worldpeacehaven@gmail.com <worldpeacehaven@gmail.com@41ca91dd-718b-b11f-0e46-dcd1c118863d>
2014-12-09 00:19:39 +0000

    hmm

--- trunk/More/iarqh.php
+++ trunk/More/iarqh.php
@@ -3,16 +3,20 @@
 error_reporting(E_ALL);
 ini_set("display_errors", true);
 $rq = $_REQUEST['data'];
-echo 'Search results for: '.$rq;
+$limit = $_REQUEST['limit'];
 $db = new PDO('mysql:host=localhost;dbname=futuqiur_iaidx;charset=utf8', 'futuqiur_iaidx', 'Artemis!');
-$query = 'SELECT * FROM `data` WHERE `filename` LIKE \'%'.$rq.'%\' LIMIT 10';
-#echo 'Running query:'.$query;
+#$query = 'SELECT * FROM `data` WHERE `filename` LIKE \'%'.$rq.'%\' LIMIT 10';
+#$query = 'SELECT * FROM `data` WHERE `filename` LIKE \'%'.$rq.'%\' LIMIT '.$limit;
+$query = 'SELECT * FROM `data` WHERE MATCH(`filename`) AGAINST(\''.$rq.'\') LIMIT '.$limit;
+echo 'Query:'.$query;
+echo 'Search results for: '.$rq;
 $results = $db->query($query);
 $resultListEntryA= '<ul><li><a href="'; # url
 $resultListEntryB= '"><b>'; #title
 $resultListEntryC = '</b><br>'; # url
 $resultListEntryD = '<br><i>SHA: '; # SHA
-$resultListEntryE = '</i></a></li></ul>';
+$resultListEntryE = '</i><br><i>Date: '; # SHA
+$resultListEntryF = '</i></a></li></ul>';
 foreach($results as $res) {
 	echo $resultListEntryA;
 	echo $res['url'];
@@ -23,5 +27,7 @@ foreach($results as $res) {
 	echo $resultListEntryD;
 	echo $res['sha'];
 	echo $resultListEntryE;
+	echo $res['date'];
+	echo $resultListEntryF;
 }
 ?>
