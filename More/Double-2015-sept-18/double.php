6ce1e2ce96c0ff7726e03003ff68bd5953c050e7
ethus3h <kolubat@gmail.com>
2015-09-19 01:39:18 -0400

    A better layout.

--- More/Double-2015-sept-18/double.php
+++ More/Double-2015-sept-18/double.php
@@ -1,7 +1,7 @@
 <?php
-echo '<!DOCTYPE html><html style="height:100%;"><head><title>"' . $_REQUEST['s'] . '" — Search Results</title></head><body style="height:100%; margin: 0px;">';
+echo '<!DOCTYPE html><html style="height:100%;"><head><title>"' . $_REQUEST['s'] . '" — Search Results</title></head><body style="height:100%; margin: 0px;overflow-y:hidden;">';
 echo '<div style="height:100%;">';
-echo '<iframe src="' . 'http://goosegoosego.com/search?q=' . $_REQUEST['s'] . '" style="height:60%;width:100%;border:0px;display:block;"></iframe>';
-echo '<iframe src="' . 'https://www.bing.com/search?q=' . $_REQUEST['s'] . '" style="height:40%;width:100%;border:0px;display:block;"></iframe>';
+echo '<iframe src="' . 'http://goosegoosego.com/search?q=' . $_REQUEST['s'] . '" style="width:60%;height:100%;border:0px;"></iframe>';
+echo '<iframe src="' . 'https://www.bing.com/search?q=' . $_REQUEST['s'] . '" style="width:40%;height:100%;border:0px;"></iframe>';
 echo '</div></body></html>';
 ?>
