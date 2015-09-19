c45444e25155f2c6bfcd86565af33f2372be42bf
ethus3h <kolubat@gmail.com>
2015-09-19 02:52:17 -0400

    Fixes for bugs — now quoted searches and DuckDuckGo !bangs work properly.

--- More/Double-2015-sept-18/double.php
+++ More/Double-2015-sept-18/double.php
@@ -1,7 +1,11 @@
 <?php
-echo '<!DOCTYPE html><html style="height:100%;"><head><title>"' . $_REQUEST['s'] . '" — Search Results</title></head><body style="height:100%; margin: 0px;overflow-y:hidden;">';
+if(substr($_REQUEST['s'],0,1) == '!') {
+	header("Location: http://goosegoosego.com/search?q=" . htmlentities($_REQUEST['s']));
+	die();
+}
+echo '<!DOCTYPE html><html style="height:100%;"><head><title>"' . htmlentities($_REQUEST['s']) . '" — Search Results</title></head><body style="height:100%; margin: 0px;overflow-y:hidden;">';
 echo '<div style="height:100%;">';
-echo '<iframe src="' . 'http://goosegoosego.com/search?q=' . $_REQUEST['s'] . '" style="width:60%;height:100%;border:0px;"></iframe>';
-echo '<iframe src="' . 'https://www.bing.com/search?q=' . $_REQUEST['s'] . '" style="width:40%;height:100%;border:0px;"></iframe>';
+echo '<iframe src="' . 'http://goosegoosego.com/search?q=' . htmlentities($_REQUEST['s']) . '" style="width:60%;height:100%;border:0px;"></iframe>';
+echo '<iframe src="' . 'https://www.bing.com/search?q=' . htmlentities($_REQUEST['s']) . '" style="width:40%;height:100%;border:0px;"></iframe>';
 echo '</div></body></html>';
 ?>
