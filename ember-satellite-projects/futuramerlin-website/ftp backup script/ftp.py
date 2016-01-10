47abbbf44a4b9d4e6856d748acab4400cc1e4e58
ethus3h <kolubat@gmail.com>
2016-01-09 20:43:40 -0500

    D'ohhhhh.

--- ember-satellite-projects/futuramerlin-website/ftp backup script/ftp.py
+++ ember-satellite-projects/futuramerlin-website/ftp backup script/ftp.py
@@ -1,123 +1,66 @@
-from distutils.version import StrictVersion
-import datetime
-import hashlib
+import sys
+import urllib.request
 import os
 import re
-import socket
-import shutil
-import time
-import sys
-import urllib
-try:
-    import requests
-except ImportError:
-    print('Please install or update the requests module.')
-    sys.exit(1)
 
-import seesaw
-from seesaw.config import realize, NumberConfigValue
-from seesaw.externalprocess import WgetDownload, ExternalProcess
-from seesaw.item import ItemInterpolation, ItemValue
-from seesaw.pipeline import Pipeline
-from seesaw.project import Project
-from seesaw.task import SimpleTask, SetItemKey, LimitConcurrent
-from seesaw.tracker import PrepareStatsForTracker, GetItemFromTracker, \
-    UploadWithTracker, SendDoneToTracker
-from seesaw.util import find_executable
+wpull_hook = globals().get('wpull_hook') # silence code checkers
+tries = 0
+max_tries = 5
+item_item = os.environ['item_item']
 
+def handle_response(url_info, record_info, response_info):
+    global tries
+    global max_tries
 
-# check the seesaw version
-if StrictVersion(seesaw.__version__) < StrictVersion("0.8.3"):
-    raise Exception("This pipeline needs seesaw version 0.8.3 or higher.")
+    response_code = response_info['response_code']
 
+    if 200 <= response_code <= 299:
+        tries = 0
+        return wpull_hook.actions.NORMAL
+    elif response_code in (530, 550):
+        tries = 0
+        return wpull_hook.actions.FINISH
+    else:
+        if tries >= max_tries:
+            raise Exception('Something went wrong, received status code %d %d times. ABORTING...'%(response_code, max_tries))
+        print('You received status code %d. Retrying...'%response_code)
+        sys.stdout.flush()
+        tries += 1
+        return wpull_hook.actions.RETRY
 
-###########################################################################
-# Find a useful Wpull executable.
-#
-# WPULL_EXE will be set to the first path that
-# 1. does not crash with --version, and
-# 2. prints the required version string
-WPULL_EXE = find_executable(
-    "Wpull",
-    re.compile(r"\b1\.2\b"),
-    [
-        "./wpull",
-        os.path.expanduser("~/.local/share/wpull-1.2/wpull"),
-        os.path.expanduser("~/.local/bin/wpull"),
-        "./wpull_bootstrap",
-        "wpull",
-    ]
-)
+def handle_error(url_info, record_info, error_info):
+    global max_tries
+    global item_item
 
-if not WPULL_EXE:
-    raise Exception("No usable Wpull found.")
+    tries = record_info['try_count']
+    if url_info['url'].endswith('/'):
+        item_file = re.search(r'^ftp:\/\/([^/]+)', url_info['url']).group(1) + '_dir_not_found'
+    else:
+        item_file = re.search(r'^ftp:\/\/([^/]+)', url_info['url']).group(1) + '_file_not_found'
 
+    item_message = urllib.request.urlopen('http://archive.org/download/{0}/{1}'.format(item_item, item_file))
+    if not item_message.getcode() in (200, 404):
+        raise Exception('You received status code %d with URL %s'%(item_list.status_code, 'https://archive.org/download/{0}/{1}'.format(item_item, item_file)))
+    item_message_text = item_message.read()
+    if '/NONEXISTINGFILEdgdjahxnedadbacxjbc' in item_message_text.decode('utf-8'):
+        item_messages = item_message_text.decode('utf-8').split('/NONEXISTINGFILEdgdjahxnedadbacxjbc')
+    else:
+        item_messages = item_message_text.decode('utf-8').split('NONEXISTINGFILEdgdjahxnedadbacxjbc')
 
-###########################################################################
-# The version number of this pipeline definition.
-#
-# Update this each time you make a non-cosmetic change.
-# It will be added to the WARC files and reported to the tracker.
-VERSION = "2016Jan09"
-TRACKER_ID = 'futuramerlin'
-# TRACKER_HOST = ''
+    if item_message.getcode() == 200:
+        try:
+            urllib.request.urlopen(url_info["url"])
+        except Exception as error:
+            error_message = str(error)
+            print("ERROR Received error message " + error_message)
+            sys.stdout.flush()
+            if all(text in error_message for text in item_messages):
+                print('INFO ' + url_info['url'] + ' does not exist, skipping...')
+                sys.stdout.flush()
+                return wpull_hook.actions.FINISH
 
+    if tries >= max_tries:
+        raise Exception('Something went wrong... ABORTING...')
 
-###########################################################################
-# This section defines project-specific tasks.
-#
-# Simple tasks (tasks that do not need any concurrency) are based on the
-# SimpleTask class and have a process(item) method that is called for
-# each item.
-
-
-class CheckIP(SimpleTask):
-    def __init__(self):
-        SimpleTask.__init__(self, "CheckIP")
-        self._counter = 0
-
-    def process(self, item):
-        if self._counter <= 0:
-            item.log_output('Checking IP address.')
-            ip_set = set()
-
-            ip_set.add(socket.gethostbyname('twitter.com'))
-            ip_set.add(socket.gethostbyname('facebook.com'))
-            ip_set.add(socket.gethostbyname('youtube.com'))
-            ip_set.add(socket.gethostbyname('microsoft.com'))
-            ip_set.add(socket.gethostbyname('icanhas.cheezburger.com'))
-            ip_set.add(socket.gethostbyname('archiveteam.org'))
-
-            if len(ip_set) != 6:
-                item.log_output('Got IP addresses: {0}'.format(ip_set))
-                item.log_output(
-                    'You are behind a firewall or proxy. That is a big no-no!')
-                raise Exception(
-                    'You are behind a firewall or proxy. That is a big no-no!')
-
-        # Check only occasionally
-        if self._counter <= 0:
-            self._counter = 10
-        else:
-            self._counter -= 1
-
-
-def get_hash(filename):
-    with open(filename, 'rb') as in_file:
-        return hashlib.sha1(in_file.read()).hexdigest()
-
-
-CWD = os.getcwd()
-PIPELINE_SHA1 = get_hash(os.path.join(CWD, 'pipeline.py'))
-SCRIPT_SHA1 = get_hash(os.path.join(CWD, 'ftp.py'))
-
-
-def stats_id_function(item):
-    # For accountability and stats.
-    d = {
-        'pipeline_hash': PIPELINE_SHA1,
-        'script_hash': SCRIPT_SHA1,
-        'python_version': sys.version,
-        }
-
-    return d
+wpull_hook.callbacks.handle_response = handle_response
+wpull_hook.callbacks.handle_error = handle_error
