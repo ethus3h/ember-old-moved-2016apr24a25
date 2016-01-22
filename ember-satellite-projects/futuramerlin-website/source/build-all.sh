7fad6825a27696ea87214387649c60832caf552f
ethus3h <kolubat@gmail.com>
2016-01-22 10:23:59 -0500

    Yay!

--- ember-satellite-projects/futuramerlin-website/source/build-all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Requires html-minifier: https://www.npmjs.com/package/html-minifier
-#Requires html5-tidy: https://github.com/htacg/tidy-html5
-#Requires minifier: https://www.npmjs.com/package/minifier
-
-./build-page-fn.sh ./*.htm;
