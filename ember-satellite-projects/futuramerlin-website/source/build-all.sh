eb0bab43dee45f08ee01300d91e8a99d9f3db63a
ethus3h <kolubat@gmail.com>
2016-01-22 10:09:52 -0500

    Why the hell does build all only do the first one

---
+++ ember-satellite-projects/futuramerlin-website/source/build-all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Requires html-minifier: https://www.npmjs.com/package/html-minifier
+#Requires html5-tidy: https://github.com/htacg/tidy-html5
+#Requires minifier: https://www.npmjs.com/package/minifier
+
+./build-page-fn.sh ./*.htm;
--- ember-satellite-projects/futuramerlin-website/source/build-page.sh
+++ ember-satellite-projects/futuramerlin-website/source/build-page.sh
@@ -3,9 +3,9 @@
 #Requires html5-tidy: https://github.com/htacg/tidy-html5
 #Requires minifier: https://www.npmjs.com/package/minifier
 
-cp -v "1_start.htm" "./built/$1.htm";
+cp -v "1_start.html" "./built/$1.htm";
 cat "$1.htm" >> "./built/$1.htm";
-cat "2_end.htm" >> "./built/$1.htm";
+cat "2_end.html" >> "./built/$1.htm";
 tidy --output "./built/$1.htm" "./built/$1.htm";
 html-minifier --output "./built/$1.htm" --remove-comments --use-short-doctype "./built/$1.htm";
 tr '\n' ' ' < "./built/$1.htm" > "./built/1.tmp";
@@ -14,6 +14,7 @@ tr '\r' ' ' < "./built/2.tmp" > "./built/3.tmp";
 tr -s " " < "./built/3.tmp" > "./built/$1.htm";
 perl -p -i -e 's/> </></g' "./built/$1.htm";
 perl -p -i -e "s/nav-item-inactive $1/nav-item-selected/g" "./built/$1.htm";
+perl -p -i -e "s/nav-item-inactive \w+/nav-item-inactive/g" "./built/$1.htm";
 rm -v ./built/*.tmp;
 
 cp -v "m.css" "./built/m.css";
