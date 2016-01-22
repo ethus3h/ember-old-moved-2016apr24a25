1fbbef870d649ef7f59b26a6f58cdeae2c5fac75
ethus3h <kolubat@gmail.com>
2016-01-22 09:42:01 -0500

    lolwut

--- ember-satellite-projects/futuramerlin-website/source/build-page.sh
+++ ember-satellite-projects/futuramerlin-website/source/build-page.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 #Requires html-minifier: https://www.npmjs.com/package/html-minifier
 #Requires html5-tidy: https://github.com/htacg/tidy-html5
-#Requires GNU sed to be in the PATH as gsed
-#On OS X, requires gsed: sudo port install gsed
+#Requires minifier: https://www.npmjs.com/package/minifier
 
-cp -v "1_start.txt" "./built/$1.htm";
-cat "$1.txt" >> "./built/$1.htm";
-cat "2_end.txt" >> "./built/$1.htm";
+cp -v "1_start.htm" "./built/$1.htm";
+cat "$1.htm" >> "./built/$1.htm";
+cat "2_end.htm" >> "./built/$1.htm";
 tidy --output "./built/$1.htm" "./built/$1.htm";
 html-minifier --output "./built/$1.htm" --remove-comments --use-short-doctype "./built/$1.htm";
 tr '\n' ' ' < "./built/$1.htm" > "./built/1.tmp";
 tr '\t' ' ' < "./built/1.tmp" > "./built/2.tmp";
 tr '\r' ' ' < "./built/2.tmp" > "./built/3.tmp";
-tr -s " " < "./built/3.tmp" > "./built/$1.htm"
+tr -s " " < "./built/3.tmp" > "./built/$1.htm";
+perl -p -i -e 's/> </></g' "./built/$1.htm";
+perl -p -i -e "s/nav-item-inactive $1/nav-item-selected/g" "./built/$1.htm";
+rm -v ./built/*.tmp;
+
+cp -v "m.css" "./built/m.css";
+tr '\n' ' ' < "./built/m.css" > "./built/1.tmp";
+tr '\t' ' ' < "./built/1.tmp" > "./built/2.tmp";
+tr '\r' ' ' < "./built/2.tmp" > "./built/3.tmp";
+tr -s " " < "./built/3.tmp" > "./built/m.css";
+minify --no-comments -o "./built/m.css" "./built/m.css";
 rm -v ./built/*.tmp;
