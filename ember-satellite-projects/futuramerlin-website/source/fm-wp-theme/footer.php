aca0233af89f599f49480fe05470fa2220f9ea39
ethus3h <kolubat@gmail.com>
2016-01-29 01:40:52 -0500

    hmm

---
+++ ember-satellite-projects/futuramerlin-website/source/fm-wp-theme/footer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package raze
+ */
+?>
+
+	</div>
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+	<div class="footer container">
+	</footer>
+
+<?php wp_footer(); ?></body>
+</html>
