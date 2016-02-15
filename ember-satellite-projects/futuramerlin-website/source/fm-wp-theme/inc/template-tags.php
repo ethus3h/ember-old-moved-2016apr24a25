7286a67a0badcb8e605e17e64495da9951eb1c3a
ethus3h <kolubat@gmail.com>
2016-02-14 20:35:05 -0500

    Use fancy page navigation for category and tag pages

--- ember-satellite-projects/futuramerlin-website/source/fm-wp-theme/inc/template-tags.php
+++ ember-satellite-projects/futuramerlin-website/source/fm-wp-theme/inc/template-tags.php
@@ -16,22 +16,7 @@ function raze_paging_nav() {
 	if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
 		return;
 	}
-	?>
-	<nav class="navigation paging-navigation">
-		<h1 class="screen-reader-text"><?php _e( 'Posts navigation', 'raze' ); ?></h1>
-		<div class="nav-links">
-
-			<?php if ( get_next_posts_link() ) : ?>
-			<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'raze' ) ); ?></div>
-			<?php endif; ?>
-
-			<?php if ( get_previous_posts_link() ) : ?>
-			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'raze' ) ); ?></div>
-			<?php endif; ?>
-
-		</div><!-- .nav-links -->
-	</nav><!-- .navigation -->
-	<?php
+	raze_nav();
 }
 endif;
 
